22f5eda328b9f2aa0096468913f1fd3bc7446481
MatNepo <71396245+MatNepo@users.noreply.github.com>
2024-01-26 00:19:29 +0300

    Add lab3 with some explanations via README.md

---
+++ classBook.py
@@ -0,0 +1,65 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    @property
+    def name(self):
+        return self.name
+
+    @property
+    def author(self):
+        return self.author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @author.setter
+    def author(self, value):
+        self._author = value
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть положительным целым числом.")
+        self._pages = value
+
+    def __str__(self):
+        return f"{super().__str__()}. Количество страниц: {self.pages}"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self._duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, (int, float)) or value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом.")
+        self._duration = value
+
+    def __str__(self):
+        return f"{super().__str__()}. Продолжительность: {self.duration} часов"
